aacd7a4124f9a5488af993b14d1ffc1e41c21c77
LimMinKyo <story1400@naver.com>
2024-01-12 11:25:55 +0900

    #1.7 Recap

--- main.dart
+++ main.dart
@@ -1,5 +1,29 @@
 void main() {
+  var name = 'name';
+  int age = 26;
+  final finalValue = 'finalValue';
+  dynamic dynamicValue = 'dynamicValue';
+  String? nullableValue = null;
+  late final String lateValue;
   const API_KEY = 'a1b2c3';
 
+  if (dynamicValue is String) {
+    print(dynamicValue.length);
+  }
+
+  dynamicValue = 12;
+
+  if (dynamicValue is int) {
+    print(dynamicValue.isEven);
+  }
+
+  lateValue = 'lateValue';
+
+  print(name);
+  print(age);
+  print(finalValue);
+  print(dynamicValue);
+  print(nullableValue?.length);
+  print(lateValue);
   print(API_KEY);
 }
